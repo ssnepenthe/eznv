e6142219ff6a6fc5b25b52ec0e462f5bcd607b78
ssnepenthe <ssnepenthe@users.noreply.github.com>
2026-03-12 15:44:46 -0700

    First pass at list command

--- src/Command/ListCommand.php
+++ src/Command/ListCommand.php
@@ -2,21 +2,26 @@
 
 namespace Eznv\Command;
 
+use Eznv\Environment;
 use Eznv\EnvironmentFinder;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 // @todo "list" name conflicts with the built-in symfony/console list command which lists all registered commands
 // but i dont really like env-list
 #[AsCommand(name: 'env-list', description: 'List all eznv environments')]
 final class ListCommand
 {
-    public function __invoke(): int
+    public function __invoke(SymfonyStyle $io): int
     {
-        $environments = (new EnvironmentFinder)->findAll();
-
-        // @todo Not sure how I want to display this and what info to include yet.
-        dump($environments);
+        $io->table(
+            ['ID', 'Name', 'Path'],
+            array_map(
+                fn (Environment $environment) => [$environment->project->id, $environment->project->name, $environment->project->path],
+                (new EnvironmentFinder)->findAll()
+            )
+        );
 
         return Command::SUCCESS;
     }
